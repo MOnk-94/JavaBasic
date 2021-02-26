e91ddc9fd5d910b91491a6072334e611ec7375c2
MOnk-94 <koentc010@naver.com>
2021-02-26 17:20:03 +0900

    21-026 완결

--- day5/Group.java
+++ day5/Group.java
@@ -1,5 +1,26 @@
 package day5;
 
+import java.util.Scanner;
+
 public class Group {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Student[] arr = new Student[3];
+		
+		for(int i=0;i<arr.length;i++) 	{
+			System.out.println("이름을 입력하세요:");
+			String nam = sc.next();
+			System.out.println("점수를 입력하세요:");
+			int sco = sc.nextInt();
+			Student sd = new Student();
+			sd.setName(nam);
+			sd.setScore(sco);		
+			arr[i] = sd;
+		}
+		for(int i=0;i<3;i++) {
+		System.out.println(arr[i].getName() + "의 점수는?  " + arr[i].getScore());
+		}
+
+	}
 
 }
--- day5/Student.java
+++ day5/Student.java
@@ -1,5 +1,23 @@
 package day5;
 
 public class Student {
+	private String name;
+	private int score;
+
+	public void setName(String nam) {
+		this.name = nam;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setScore(int sco) {
+		this.score = sco;
+	}
+
+	public int getScore() {
+		return this.score;
+	}
 
 }
